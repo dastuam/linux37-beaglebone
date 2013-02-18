8ca3b5e5fbd8d185c49d368984847616b30dd257
dastua <dastua@debian.(none)>
2013-02-18 00:38:34 -0600

    Fixed naming issues of patch folder

--- beaglebone/patch-beagle/patch-apply.sh
+++ beaglebone/patch-beagle/patch-apply.sh
@@ -4,8 +4,26 @@
 # Order matters!
 PATCHSET="dma cpsw pinctrl cpufreq adc pwm i2c usb da8xx-fb mmc fixes f2fs 6lowpan capebus hdmi pruss arm omap omap_sakoman omap_beagle_expansion omap_beagle omap_panda omap_sgx omap_fixes omap_thermal net spi capes"
 
+echo "#############################"
+echo "Unpacking patches"
+echo "#############################"
+tar xzvf patch-beagle.tgz
+
+
+echo "#############################"
+echo "#############################"
+echo "     INIT apply patches"
+echo "#############################"
+echo "#############################"
 # apply patches
 for patchset in ${PATCHSET} ; do
 	git am ${patchset}/*.patch
 done
+
+
+echo "#############################"
+echo "#############################"
+echo "     DONE apply patches"
+echo "#############################"
+echo "#############################"
 
